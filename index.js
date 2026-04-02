a526703af36bd36a75e6544a26958f13062e2044
NaveenRathore5094 <rathorenaveen121@gmail.com>
2026-04-02 17:33:55 +0530

    updated

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const server = http.createServer(app);
 const path = require("path");
 const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
+const axios= require('axios')
 
 // Disable ETag
 app.set("etag", false);
@@ -17,12 +18,7 @@ app.set("view engine", "ejs");
 app.set("trust proxy", 1);
 app.set("view cache", false);
 
-// Live reload (development)
-const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, "public"));
-liveReloadServer.watch(path.join(__dirname, "views"));
 
-app.use(connectLiveReload());
 
 // Rate limit
 const limiter = rateLimit({
@@ -47,7 +43,7 @@ app.use(
         "'self'",
         "'unsafe-inline'",
         "https://cdn.tailwindcss.com",
-        "http://localhost:35729"
+        "http://localhost:35730"
       ],
       styleSrc: ["'self'", "'unsafe-inline'"],
       imgSrc: ["'self'", "data:", "https:"]
@@ -74,9 +70,6 @@ app.use(
 
 // Routes
 
-app.get("/", (req, res) => {
-  res.render("Home")
-});
 
 app.get("/help", (req, res) => {
   res.render("help");
@@ -103,6 +96,67 @@ app.get("/star_sport_1_live_HD_ipl", (req, res) => {
 // Ping route
 app.get("/ping", (req, res) => {
   res.send("Server is alive 🚀");
+});
+
+// match api data
+app.get("/", async (req, res) => {
+
+  const options = {
+    method: 'GET',
+    url: 'https://cricbuzz-cricket.p.rapidapi.com/matches/v1/upcoming',
+    headers: {
+      'x-rapidapi-key': process.env.CricApi,
+      'x-rapidapi-host': 'cricbuzz-cricket.p.rapidapi.com'
+    }
+  };
+
+  try {
+
+    const response = await axios.request(options);
+
+    const data = response.data.typeMatches;
+
+    let matches = [];
+
+    data.forEach(type => {
+
+      type.seriesMatches?.forEach(series => {
+
+        if (series.seriesAdWrapper?.seriesName === "Indian Premier League 2026") {
+
+          series.seriesAdWrapper.matches.forEach(match => {
+
+            matches.push({
+              team1: match.matchInfo.team1.teamSName,
+              team2: match.matchInfo.team2.teamSName,
+
+              team1Img: `https://static.cricbuzz.com/a/img/v1/75x75/i1/c${match.matchInfo.team1.imageId}/team.jpg`,
+              team2Img: `https://static.cricbuzz.com/a/img/v1/75x75/i1/c${match.matchInfo.team2.imageId}/team.jpg`,
+
+              venue: match.matchInfo.venueInfo.ground,
+              city: match.matchInfo.venueInfo.city,
+
+              seriesName: match.matchInfo.seriesName,
+              matchDesc: match.matchInfo.matchDesc,
+
+              date: new Date(parseInt(match.matchInfo.startDate)).toLocaleString(),
+              startDate: parseInt(match.matchInfo.startDate)
+            });
+
+          });
+
+        }
+
+      });
+
+    });
+
+    res.render("Home", { matches });
+
+  } catch (error) {
+    console.log(error);
+  }
+
 });
 
 // Server start
